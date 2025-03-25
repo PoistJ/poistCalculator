d9cd6b1b0a05c34de0c94ad9b32cf61ff671f87f
PoistJ <157406020+PoistJ@users.noreply.github.com>
2025-03-24 23:55:58 -0400

    Added four basic mathematical functions, an operate function, populated html with containers for calculator

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(a,b) {
+    return a + b;
+};
+
+function subtract(a,b) {
+    return a - b;
+};
+
+function multiply(a,b) {
+    return a * b;
+};
+
+function divide(a,b) {
+    return a / b;
+};
+
+function operate(a,b,op) {
+    if (op == "+") {
+        return add(a,b);
+    } else if (op == "-") {
+        return subtract(a,b);
+    } else if (op == "*") {
+        return multiply(a,b);
+    } else if (op == "/") {
+        return divide(a,b);
+    }
+}
+
+let a
+let b
+let op
+let result
